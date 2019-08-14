78afefa516b13d0876bc027b419d4f1620d8fa03
eralpkor <eralp.ny@gmail.com>
2019-08-14 12:05:28 -0400

    added scss and bulma css libraries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import './App.sass';
 
 function App() {
   return (
@@ -8,6 +9,29 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
+
+      <div className="field">
+  <p className="control">
+    <span className="select">
+      <select>
+        <option>Select dropdown</option>
+      </select>
+    </span>
+  </p>
+</div>
+
+<div className="field">
+  <div className="control">
+    <input className="input" type="text" placeholder="Input" />
+  </div>
+</div>
+
+<div className="buttons">
+  <a className="button is-primary">Primary</a>
+  <a className="button is-link">Link</a>
+</div>
+
     </div>
   );
 }
